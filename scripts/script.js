ef72b9e92c694da382e4913078fbdc9cefb57be0
stem-mal <kevin.1021082@stemoctober.moe.edu.eg>
2023-07-10 03:10:20 +0300

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,16 @@
-let slideIndex = 1;
+const text = "STEM Model Arab League is the first Arab debating model in the STEM community.";
+const h2Element = document.getElementById("auto-type");
+let index = 0;
+function typeWriter() {
+  if (index < text.length) {
+    h2Element.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeWriter, 50);
+  }
+}
+
+typeWriter();
+/*let slideIndex = 1;
 showSlides(slideIndex);
 var timeout
 // Next/previous controls
@@ -49,5 +61,8 @@ function automatic() {
 }
 
 automatic()
+*/
+
+
 
 
